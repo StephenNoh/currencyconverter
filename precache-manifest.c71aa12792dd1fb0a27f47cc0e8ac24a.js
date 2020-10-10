76ebe3cdde4160b45aecbe54d92f32755280fbb1
StephenNoh <Stephen.Noh@gmail.com>
2020-10-10 18:31:37 -0500

    Updates

---
+++ precache-manifest.c71aa12792dd1fb0a27f47cc0e8ac24a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727c4a168dc9416cc6d746051e0998a3",
+    "url": "/currencyconverter/index.html"
+  },
+  {
+    "revision": "ba0c0b2ed7009cff15d4",
+    "url": "/currencyconverter/static/css/main.36904f70.chunk.css"
+  },
+  {
+    "revision": "cb3342f4068de1c7f498",
+    "url": "/currencyconverter/static/js/2.e9394231.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currencyconverter/static/js/2.e9394231.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba0c0b2ed7009cff15d4",
+    "url": "/currencyconverter/static/js/main.af7e29e2.chunk.js"
+  },
+  {
+    "revision": "8fb57f8c35d303d593b8",
+    "url": "/currencyconverter/static/js/runtime-main.a977116a.js"
+  }
+]);
